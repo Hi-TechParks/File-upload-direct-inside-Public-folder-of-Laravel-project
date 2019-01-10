ef130fd2e5376d4701c367953ad4a8d1eca1ebae
Md Habibur Rahman <mhrahman@biziitech.com>
2019-01-10 16:44:51 +0600

    update

--- file-upload-controller.php
+++ file-upload-controller.php
@@ -1,5 +1,7 @@
+<?php 
 
-// File upload
+
+// File upload into Public folder
 if($request->hasFile('resource')){
     $filenameWithExt = $request->file('resource')->getClientOriginalName();
     
@@ -14,3 +16,23 @@ if($request->hasFile('resource')){
 else{
     $fileNameToStore = 'noimage.jpg'; // if no image selected this will be the default image
 }
+
+
+
+
+// image upload into Storage folder
+if($request->hasFile('blog_image')){
+    $filenameWithExt = $request->file('blog_image')->getClientOriginalName();
+    $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME); 
+    $extension = $request->file('blog_image')->getClientOriginalExtension();
+    $fileNameToStore = $filename.'_'.time().'.'.$extension;
+    
+    // File upload inside storage/app/public/images/blog folder
+    $path = $request->file('blog_image')->storeAs('public/images/blog', $fileNameToStore);
+}
+else{
+    $fileNameToStore = 'noimage.jpg'; // if no image selected this will be the default image
+}
+
+
+?>
